0d409f6253368b00c8a654cd7acfc941c84ce44f
Jorge Linares <jorge.linares8189@alumnos.udg.mx>
2020-10-04 14:20:47 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -8,37 +8,35 @@ int main()
     string cadena;
 
     getline(cin, cadena);
-    //cout << cadena.size() << endl;
-    //cout << sizeof(cadena) << endl;
 
     if (cadena == "float")
     {
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(float) << endl;
     }
     else if (cadena == "char")
     {
 
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(char) << endl;
     }
     else if (cadena == "int")
     {
 
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(int) << endl;
     }
     else if (cadena == "short")
     {
 
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(short) << endl;
     }
     else if (cadena == "double")
     {
 
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(double) << endl;
     }
     else if (cadena == "long")
     {
 
-        cout << sizeof(cadena) << endl;
+        cout << sizeof(long) << endl;
     }
     return 0;
 }
