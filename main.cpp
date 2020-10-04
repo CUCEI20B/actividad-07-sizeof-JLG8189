c208373819a3477de45b0464d2b1e2a5dd8a63b5
Jorge Linares <jorge.linares8189@alumnos.udg.mx>
2020-10-04 14:04:51 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
 
+    string cadena;
+    int a;
+    float b;
+    short c;
+    double d;
+    long e;
+
+    getline(cin, cadena);
+    //cout << cadena.size() << endl;
+    cout << sizeof(cadena) << endl;
+
+    cin >> a;
+    cout << sizeof(a) << endl;
+
+    cin >> b;
+    cout << sizeof(b) << endl;
+
+    cin >> c;
+    cout << sizeof(c) << endl;
+
+    cin >> d;
+    cout << sizeof(d) << endl;
+    
+    cin >> e;
+    cout << sizeof(d) << endl;
+
+    
     return 0;
 }
